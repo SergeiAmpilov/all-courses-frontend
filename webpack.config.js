c061537e8296719c00e87b838500451fe7ab0494
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-30 22:29:17 +0300

    feat: html config for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { 
@@ -16,4 +18,19 @@ module.exports = {
     port: 8080,
     open: true
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
 }
