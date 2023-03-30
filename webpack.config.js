7c70e56574cd46b9423a6291e0cdcb25ccc88e44
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-30 23:01:22 +0300

    feat: setup css for babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
   entry: { 
@@ -24,13 +24,19 @@ module.exports = {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        'postcss-loader']
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
   ],
 }
